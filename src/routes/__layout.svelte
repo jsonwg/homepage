03b77f759208168c2eff03cf0215e008e3b2987d
Jason <ja2415n@gmail.com>
2021-11-15 22:00:00 -0800

    Add header and body

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,21 @@
 <script>
-	import '../app.css';
+  import '../app.css';
+  import { onMount } from 'svelte';
+  import { blur } from 'svelte/transition';
+
+  let ready = false;
+  onMount(() => (ready = true));
 </script>
 
-<slot />
+{#if ready}
+  <div
+    class="bg-white flex flex-col place-content-between h-screen"
+    transition:blur={{ duration: 1000 }}
+  >
+    <header class="flex justify-center w-full p-2 bg-blue-900 shadow-2xl">
+      <h1 class="inline-block text-4xl md:text-8xl text-white tracking-tighter">PROJECT CODE</h1>
+    </header>
+    <div class="h-4/5 bg-blue-900 m-10 rounded-2xl shadow-2xl" />
+    <slot />
+  </div>
+{/if}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,33 @@
+const colors = require('tailwindcss/colors');
+
 const config = {
-	mode: 'jit',
-	purge: ['./src/**/*.{html,js,svelte,ts}'],
+  mode: 'jit',
+  purge: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      gray: colors.gray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      blue: colors.blue,
+      sky: colors.sky,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      black: '#0E0E0E',
+      white: '#FFFFFF',
+    },
+  },
 
-	plugins: []
+  plugins: [],
 };
 
 module.exports = config;
